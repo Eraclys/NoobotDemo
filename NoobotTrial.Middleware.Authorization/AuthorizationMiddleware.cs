cf960083b4af17efea1a87f2dbf109447a840618
Milian Lichere <milian.lichere@investec.co.uk>
2018-06-29 21:42:04 +0100

    improved proof of concept

--- NoobotTrial.Middleware.Authorization/AuthorizationMiddleware.cs
+++ NoobotTrial.Middleware.Authorization/AuthorizationMiddleware.cs
@@ -1,75 +1,132 @@
-﻿using Noobot.Core.MessagingPipeline.Middleware;
+﻿using Newtonsoft.Json;
+using Noobot.Core.MessagingPipeline.Middleware;
+using Noobot.Core.MessagingPipeline.Middleware.ValidHandles;
 using Noobot.Core.MessagingPipeline.Request;
 using Noobot.Core.MessagingPipeline.Response;
+using NoobotTrial.Core;
 using System.Collections.Generic;
 
 namespace NoobotTrial.Middleware.Authorization
 {
-    public class AuthorizationMiddleware : IMiddleware
+    public class AuthorizationMiddleware : MiddlewareBase
     {
         private readonly AuthorizationPlugin _authorizationPlugin;
-        private readonly IMiddleware _next;
 
-        public AuthorizationMiddleware(AuthorizationPlugin authorizationPlugin, IMiddleware next)
+        public AuthorizationMiddleware(AuthorizationPlugin authorizationPlugin, IMiddleware next) : base(next)
         {
             _authorizationPlugin = authorizationPlugin;
-            _next = next;
-        }
 
-        protected internal IEnumerable<ResponseMessage> Next(IncomingMessage message)
-        {
-            return _next?.Invoke(message) ?? new ResponseMessage[0];
-        }
-
-        public IEnumerable<ResponseMessage> Invoke(IncomingMessage message)
-        {
-            var userEmail = message.UserEmail;
-
-            if (message.TargetedText == "authorize all")
+            HandlerMappings = new[]
             {
-                _authorizationPlugin.GrantAccess(userEmail);
-                yield return message.ReplyDirectlyToUser("Access granted to everyone.");
-                yield break;
-            }
-
-            if (message.TargetedText == "deny all")
-            {
-                _authorizationPlugin.DenyAccess(userEmail);
-                yield return message.ReplyDirectlyToUser("Access denied to everyone.");
-                yield break;
-            }
-
-            if (_authorizationPlugin.IsAuthorizedToExecuteCommands(userEmail))
-            {
-                foreach (ResponseMessage responseMessage in Next(message))
+                new HandlerMapping
                 {
-                    yield return responseMessage;
+                    ValidHandles = ValidHandle.For(
+                        "auth (grant|deny) [permissionName] [userEmail]", 
+                        x => PermissionCommandParser.TryParsePermissionCommandExpression(x).WasSuccessful),
+                    Description = "Grants or denies access to a command",
+                    EvaluatorFunc = HandlePermissionCommand,
+                },
+                new HandlerMapping
+                {
+                    ValidHandles = ExactMatchHandle.For("auth ls -all"),
+                    Description = "Will provide the full list of permissions",
+                    EvaluatorFunc = HandleListAllPermissions
+                },
+                new HandlerMapping
+                {
+                    ValidHandles = ValidHandle.For(
+                        "auth ls -u [userEmail]",
+                        x => PermissionCommandParser.TryParsePermissionsByUserExpression(x).WasSuccessful),
+                    Description = "Will provide the full list of permissions for a user",
+                    EvaluatorFunc = HandleListPermissionsByUser
+                },
+                new HandlerMapping
+                {
+                    ValidHandles = ValidHandle.For(
+                        "auth ls -p [permissionName]",
+                        x => PermissionCommandParser.TryParseUsersByPermissionExpression(x).WasSuccessful),
+                    Description = "Will provide the full list of users who have the specified permission",
+                    EvaluatorFunc = HandleListUsersByPermissions
                 }
-            }
-            else
+            };
+        }
+
+        private IEnumerable<ResponseMessage> HandleListUsersByPermissions(IncomingMessage incomingMessage, IValidHandle validHandle)
+        {
+            if (!HasAccessToAuthorizationModule(incomingMessage))
             {
-                yield return message.ReplyDirectlyToUser("Access Denied.");
+                yield return DeniedMessage(incomingMessage); yield break;
+            }
+            
+            var userEmail = PermissionCommandParser.ParseUsersByPermissionExpression(incomingMessage.TargetedText);
+
+            var allPermissions = _authorizationPlugin.ListUsersByPermissionName(userEmail);
+
+            var json = JsonConvert.SerializeObject(allPermissions, Formatting.Indented);
+
+            yield return incomingMessage.ReplyToChannel(json);
+        }
+
+        private IEnumerable<ResponseMessage> HandleListPermissionsByUser(IncomingMessage incomingMessage, IValidHandle validHandle)
+        {
+            if (!HasAccessToAuthorizationModule(incomingMessage))
+            {
+                yield return DeniedMessage(incomingMessage); yield break;
+            }
+
+            var permissionName = PermissionCommandParser.ParsePermissionsByUserExpression(incomingMessage.TargetedText);
+
+            var allPermissions = _authorizationPlugin.ListPermissionsByUserEmail(permissionName);
+
+            var json = JsonConvert.SerializeObject(allPermissions, Formatting.Indented);
+
+            yield return incomingMessage.ReplyToChannel(json);
+        }
+
+        private IEnumerable<ResponseMessage> HandleListAllPermissions(IncomingMessage incomingMessage, IValidHandle validHandle)
+        {
+            if (!HasAccessToAuthorizationModule(incomingMessage))
+            {
+                yield return DeniedMessage(incomingMessage); yield break;
+            }
+
+            var allPermissions = _authorizationPlugin.GetAllPermissions();
+            var json = JsonConvert.SerializeObject(allPermissions, Formatting.Indented);
+
+            yield return incomingMessage.ReplyToChannel(json);
+        }
+
+        private IEnumerable<ResponseMessage> HandlePermissionCommand(IncomingMessage incomingMessage, IValidHandle validHandle)
+        {
+            var command = PermissionCommandParser.ParsePermissionCommandExpression(incomingMessage.TargetedText);
+
+            if (!HasAccessToAuthorizationModule(incomingMessage))
+            {
+                yield return DeniedMessage(incomingMessage); yield break;
+            }
+            
+            _authorizationPlugin.Execute(command);
+            
+            switch (command.OperationType)
+            {
+
+                case OperationType.Grant:
+                    yield return incomingMessage.ReplyToChannel("Permission(s) granted.");
+                    break;
+                case OperationType.Deny:
+                    yield return incomingMessage.ReplyToChannel("Permission(s) revoked.");
+                    break;
             }
         }
 
-        public IEnumerable<CommandDescription> GetSupportedCommands()
+        private bool HasAccessToAuthorizationModule(IncomingMessage incomingMessage)
         {
-            yield return new CommandDescription
-            {
-                Command = "`authorize all`",
-                Description = "Grants everyone access"
-            };
+            return _authorizationPlugin.HasPermission("auth", incomingMessage.UserEmail);
+        }
 
-            yield return new CommandDescription
-            {
-                Command = "`deny all`",
-                Description = "Denies access to everyone"
-            };
-
-            foreach (var commandDescription in _next.GetSupportedCommands())
-            {
-                yield return commandDescription;
-            }
+        private static ResponseMessage DeniedMessage(IncomingMessage incomingMessage)
+        {
+            return incomingMessage.ReplyToChannel("Not on my watch!");
         }
     }
 }
--- NoobotTrial.Middleware.Authorization/AuthorizationPlugin.cs
+++ NoobotTrial.Middleware.Authorization/AuthorizationPlugin.cs
@@ -1,33 +1,103 @@
-﻿using Noobot.Core.Plugins;
+﻿using Newtonsoft.Json;
+using Noobot.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace NoobotTrial.Middleware.Authorization
 {
     public class AuthorizationPlugin : IPlugin
     {
-        private bool _isAuthorized = false;
+        private static readonly string PermissionsStoreFileName = $"{Environment.CurrentDirectory}\\Authorization.Permissions.json";
+        private static readonly object SyncRoot = new object();
+
+        protected Lazy<PermissionCollection> Permissions;
+
+        public AuthorizationPlugin()
+        {
+            Permissions = new Lazy<PermissionCollection>(Read);
+        }
+
+        public void Execute(PermissionCommand command)
+        {
+            var permissions = command.UserEmails?
+                .Select(email => new Permission(command.PermissionName, email))
+                .ToArray();
+
+            switch (command.OperationType)
+            {
+                case OperationType.Grant:
+                    Permissions.Value.Add(permissions);
+                    break;
+                case OperationType.Deny:
+                    Permissions.Value.Remove(permissions);
+                    break;
+            }
+
+            Save();
+        }
+
+        public IEnumerable<string> ListUsersByPermissionName(string permissionName)
+        {
+            return Permissions.Value
+                .Where(x => x.Name == permissionName)
+                .Select(x => x.User);
+        }
+
+        public IEnumerable<string> ListPermissionsByUserEmail(string userEmail)
+        {
+            return Permissions.Value
+                .Where(x => x.User == userEmail)
+                .Select(x => x.Name);
+        }
+
+        public bool HasPermission(string permissionName, string command)
+        {
+            return Permissions.Value.Contains(new Permission(permissionName, command));
+        }
+
+        public IEnumerable<Permission> GetAllPermissions()
+        {
+            return Permissions.Value.ToList().AsReadOnly();
+        }
+
+        private static PermissionCollection Read()
+        {
+            if (File.Exists(PermissionsStoreFileName))
+            {
+                var file = File.ReadAllText(PermissionsStoreFileName);
+
+                if (!string.IsNullOrEmpty(file))
+                {
+                    return new PermissionCollection(JsonConvert.DeserializeObject<IEnumerable<Permission>>(file));
+                }
+            }
+
+            return new PermissionCollection(Enumerable.Empty<Permission>());
+        }
+
+        private void Save()
+        {
+            var json = JsonConvert.SerializeObject(Permissions.Value.AsEnumerable(), Formatting.Indented);
+
+            lock (SyncRoot)
+            {
+                if (!File.Exists(PermissionsStoreFileName))
+                {
+                    File.Create(PermissionsStoreFileName);
+                }
+
+                File.WriteAllText(PermissionsStoreFileName, json);
+            }
+        }
 
         public void Start()
         {
-            // load config
         }
 
         public void Stop()
         {
-        }
-
-        public bool IsAuthorizedToExecuteCommands(string userEmail)
-        {
-            return _isAuthorized;
-        }
-
-        public void GrantAccess(string userEmail)
-        {
-            _isAuthorized = true;
-        }
-
-        public void DenyAccess(string userEmail)
-        {
-            _isAuthorized = false;
         }
     }
 }
