55ed258b543198377c6b9a6ab3ba0f950e43c4e1
Milian Lichere <milian.lichere@investec.co.uk>
2018-06-29 15:53:39 +0100

    POC AuthorizationPlugin

---
+++ NoobotTrial.Middleware.Authorization/AuthorizationMiddleware.cs
@@ -0,0 +1,75 @@
+﻿using Noobot.Core.MessagingPipeline.Middleware;
+using Noobot.Core.MessagingPipeline.Request;
+using Noobot.Core.MessagingPipeline.Response;
+using System.Collections.Generic;
+
+namespace NoobotTrial.Middleware.Authorization
+{
+    public class AuthorizationMiddleware : IMiddleware
+    {
+        private readonly AuthorizationPlugin _authorizationPlugin;
+        private readonly IMiddleware _next;
+
+        public AuthorizationMiddleware(AuthorizationPlugin authorizationPlugin, IMiddleware next)
+        {
+            _authorizationPlugin = authorizationPlugin;
+            _next = next;
+        }
+
+        protected internal IEnumerable<ResponseMessage> Next(IncomingMessage message)
+        {
+            return _next?.Invoke(message) ?? new ResponseMessage[0];
+        }
+
+        public IEnumerable<ResponseMessage> Invoke(IncomingMessage message)
+        {
+            var userEmail = message.UserEmail;
+
+            if (message.TargetedText == "authorize all")
+            {
+                _authorizationPlugin.GrantAccess(userEmail);
+                yield return message.ReplyDirectlyToUser("Access granted to everyone.");
+                yield break;
+            }
+
+            if (message.TargetedText == "deny all")
+            {
+                _authorizationPlugin.DenyAccess(userEmail);
+                yield return message.ReplyDirectlyToUser("Access denied to everyone.");
+                yield break;
+            }
+
+            if (_authorizationPlugin.IsAuthorizedToExecuteCommands(userEmail))
+            {
+                foreach (ResponseMessage responseMessage in Next(message))
+                {
+                    yield return responseMessage;
+                }
+            }
+            else
+            {
+                yield return message.ReplyDirectlyToUser("Access Denied.");
+            }
+        }
+
+        public IEnumerable<CommandDescription> GetSupportedCommands()
+        {
+            yield return new CommandDescription
+            {
+                Command = "`authorize all`",
+                Description = "Grants everyone access"
+            };
+
+            yield return new CommandDescription
+            {
+                Command = "`deny all`",
+                Description = "Denies access to everyone"
+            };
+
+            foreach (var commandDescription in _next.GetSupportedCommands())
+            {
+                yield return commandDescription;
+            }
+        }
+    }
+}
---
+++ NoobotTrial.Middleware.Authorization/AuthorizationPlugin.cs
@@ -0,0 +1,33 @@
+﻿using Noobot.Core.Plugins;
+
+namespace NoobotTrial.Middleware.Authorization
+{
+    public class AuthorizationPlugin : IPlugin
+    {
+        private bool _isAuthorized = false;
+
+        public void Start()
+        {
+            // load config
+        }
+
+        public void Stop()
+        {
+        }
+
+        public bool IsAuthorizedToExecuteCommands(string userEmail)
+        {
+            return _isAuthorized;
+        }
+
+        public void GrantAccess(string userEmail)
+        {
+            _isAuthorized = true;
+        }
+
+        public void DenyAccess(string userEmail)
+        {
+            _isAuthorized = false;
+        }
+    }
+}
